8cfff4954a56aaad18e1f85e3b02c609f200d877
dominick <dominickugas@gmail.com>
2021-09-20 16:33:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let arr1 = []
+    for (let i = 0; i < names.length; i++) {
+        arr1.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return arr1
+}
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise")
+
+function countDown(num) {
+
+    while (num >= 0) {
+        console.log(num--);
+
+    }
+    return num
+}
+countDown(11)
